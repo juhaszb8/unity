7bd3a0dc88d5071800826c83fea8c52bee2c310e
Juhász Barnabás <barnus.juhasz@gmail.com>
2020-01-09 23:27:05 +0100

    test upload

---
+++ Laser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    //speed változó 8
+    [SerializeField]
+    private float _speed = 8.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //laser lövedék megy felfelé
+        transform.Translate(Vector3.up * _speed* Time.deltaTime);
+
+        //ha a lövedék pozíciója > mint 8 akkor el kell puszíteni
+        if (transform.position.y > 8) {
+            Destroy(this.gameObject);
+        }
+    }
+}
